04a29a06d205cd3118b740950c1250bd75af39a1
RJ Russell <ryan.russell001@yahoo.com>
2017-05-31 14:26:12 -0700

    refactor menus

--- chord-gen.hs
+++ chord-gen.hs
@@ -110,12 +110,13 @@ buildTab nt = do
     return (b ++ genSpaces maxLen b ++ "-|")
     where
         buildTabStrings :: [(SingleNote, Steps)] -> [String]
-        buildTabStrings xs = [formatNote (fst x) ++ "||" ++ showPositions (snd x) | x <- xs]
+        buildTabStrings xs =
+            [formatNote (fst x) ++ "||" ++ showPositions (snd x) | x <- xs]
 
         showPositions :: Steps -> String
         showPositions [] = "--x"
         showPositions [x] = if x < 10 then "--" ++ show x
-                          else "-" ++ show x
+                            else "-" ++ show x
         showPositions (x:xs) = showPositions [x] ++ showPositions xs
 
         genSpaces :: Int -> String -> String
@@ -175,13 +176,12 @@ fretFooter maxFret = do
     "   0   " ++ intercalate "     " (map show (fst prt))
      ++ "     " ++ intercalate "    " (map show (snd prt)) ++ "\n"
 
-
 -- =========================================================
 -- Functions to construct diagrams.
 -- =========================================================
 -- displayBoards chromatic scale for each of the strings based on the tuning.
-makeGuitar :: Notes -> IO()
-makeGuitar tuning = do
+buildGuitar :: Notes -> IO()
+buildGuitar tuning = do
     clearScreen
     let maxFret = 12
     putStrLn $ "\n" ++ unlines (map putNotes (allNotes maxFret))
@@ -189,61 +189,60 @@ makeGuitar tuning = do
         allNotes maxFret = [chromaticScale maxFret n | n <- tuning]
 
 -- displayBoards notes for one position for a chord/scale based on the given parameters.
-makeOne :: Notes -> (SingleNote -> Notes) -> SingleNote -> IO()
-makeOne tuning scale root = do
+buildSingle :: Notes -> (SingleNote -> Notes) -> SingleNote -> IO()
+buildSingle tuning scale root = do
     clearScreen
     fret <- getFret
     let maxFret = fret + 4
         nt = zip tuning (fingering2 tuning (scale root) fret maxFret)
-    optionsMenu nt root fret maxFret
-    where
-        -- NOTE: How can I take these args out? The variables are local defined above?
-        optionsMenu :: [(SingleNote, Steps)] -> SingleNote -> Int -> Int -> IO()
-        optionsMenu nt root fret maxFret = do
-            putStr "\n1. Tab\n2. Symbols\n3. Notes\n4. Re-enter minimum fret\nChoose an option: "
-            choice <- getLine
-            case choice of
-                "1" ->
-                    putStrLn (fretHeader root fret) >> mapM_ putStrLn (buildTab nt)
-                "2" ->
-                    wrapBoard root fret maxFret (buildFretSymbols nt maxFret)
-                "3" ->
-                    wrapBoard root fret maxFret (buildFretNotes root nt maxFret)
-                "4" -> makeOne tuning scale root
-                _ -> optionsMenu nt root fret maxFret
+        wrap = wrapBoard root fret maxFret
+    menu [("1", "Tab", mapM_ putStrLn (buildTab nt)),
+          ("2", "Symbols", wrap(buildFretSymbols nt maxFret)),
+          ("3", "Notes", wrap(buildFretNotes root nt maxFret))]
 
--- displayBoards all notes for a chord/scale, from the fret passed in to the 16th fret.
-makeAll :: Notes -> (SingleNote -> Notes) -> SingleNote -> IO()
-makeAll tuning scale root = do
-    let maxFret = 16
-        fret = 0
+-- Generates all notes for a chord/scale, from the fret passed in to the 16th fret.
+buildAll :: Notes -> (SingleNote -> Notes) -> SingleNote -> IO()
+buildAll tuning scale root = do
+    clearScreen
+    let fret = 0
+        maxFret = 16
         nt = zip tuning (fingering tuning (scale root) fret maxFret)
-    putStr "\n1. Symbols\n2. Notes\nChoose an option: "
-    choice <- getLine
-    case choice of
-        "1" ->
-            wrapBoard root fret maxFret (buildFretSymbols nt maxFret)
-        "2" ->
-            wrapBoard root fret maxFret (buildFretNotes root nt maxFret)
-        _ -> makeAll tuning scale root
+        wrap = wrapBoard root fret maxFret
+    menu [("1", "Symbols", wrap (buildFretSymbols nt maxFret)),
+          ("2", "Notes", wrap(buildFretNotes root nt maxFret))]
 
 -- =========================================================
 -- Helper functions for IO actions.
 -- =========================================================
+menu :: [(String, String, IO())] -> IO()
+menu opts = do
+    putStr . unlines $ map fopts opts
+    putStr "Enter choice: "
+    choice <- getLine
+    case [cmd | (n, l, cmd) <- opts, n == choice] of
+            [] -> do
+                putStrLn "\nInvalid option...Try again!\n"
+                menu opts
+            (cmd:_) -> cmd
+    where
+        fopts (n, l, _) = n ++ ". " ++ l
+
 getFret :: IO Int
 getFret = do
     putStr "\nEnter minimum fret (0 - 12): "
     maybeFret <- getLine
     case readMaybe maybeFret of
         Just fret | fret >= 0 && fret <= 12 -> return fret
-        _ -> getFret
+        _ -> do
+            putStrLn "\nInvalid fret choice...Try again!"
+            getFret
+
+wrapBoard :: SingleNote -> Int -> Int -> [String] -> IO()
+wrapBoard root fret maxFret result =
+    displayBoard([fretHeader root fret] ++ result ++ [fretFooter maxFret])
 
 displayBoard :: [String] -> IO()
 displayBoard = mapM_ putStrLn
-
-wrapBoard :: SingleNote -> Int -> Int -> [String] -> IO()
-wrapBoard root fret maxFret output =
-    displayBoard([fretHeader root fret] ++ output ++ [fretFooter maxFret])
 
 clearScreen :: IO()
 clearScreen = putStr "\ESC[2J"
