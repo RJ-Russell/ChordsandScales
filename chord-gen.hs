6b9c9be64707abfd254261ec1a6c6ebb9fa957c7
RJ Russell <ryan.russell001@yahoo.com>
2017-06-03 02:47:45 -0700

    refactor and reorder functions

--- chord-gen.hs
+++ chord-gen.hs
@@ -26,21 +26,18 @@ halfStepU Ab = A
 halfStepU n = succ n
 
 -- Returns a SingleNote that is `n` steps from the SingleNote passed in.
-xSteps :: SingleNote -> Int -> SingleNote
-xSteps n x = iterate halfStepU n !! x
+nSteps :: SingleNote -> Int -> SingleNote
+nSteps base numSteps = iterate halfStepU base !! numSteps
+
+-- Generates a chromatic scale using the SingleNote passed in as the starting point.
+chromatic :: SingleNote -> Int -> Notes
+chromatic base maxFret = take (1 + maxFret) (iterate halfStepU base)
 
 -- ====== SCALES =================================
 -- Generates a scale of SingleNotes, using the SingleNote argument
 -- as the starting point and the a predefined array of int steps as the scale.
 genScale :: SingleNote -> Steps -> Notes
-genScale n ss = [xSteps n s | s <- ss]
-
--- Scales.
-diatonicMaj, diatonicMin, pentatonicMaj, pentatonicMin :: SingleNote -> Notes
-diatonicMaj root = genScale root [0, 2, 4, 5, 7, 9, 11, 12]
-diatonicMin root = genScale root [0, 2, 3, 5, 7, 8, 10, 12]
-pentatonicMaj root = genScale root [0, 2, 4, 7, 9, 12]
-pentatonicMin root = genScale root [0, 3, 5, 7, 10, 12]
+genScale root ss = [nSteps root s | s <- ss]
 
 -- Scales for chord construction.
 majTriad, minTriad, maj7th, min7th, sus4, sus2 :: SingleNote -> Notes
@@ -51,8 +48,15 @@ min7th root = genScale root [0, 3, 7, 10]
 sus4 root = genScale root [0, 5, 7]
 sus2 root = genScale root [0, 1, 7]
 
+-- Scales.
+diatonicMaj, diatonicMin, pentatonicMaj, pentatonicMin :: SingleNote -> Notes
+diatonicMaj root = genScale root [0, 2, 4, 5, 7, 9, 11, 12]
+diatonicMin root = genScale root [0, 2, 3, 5, 7, 8, 10, 12]
+pentatonicMaj root = genScale root [0, 2, 4, 7, 9, 12]
+pentatonicMin root = genScale root [0, 3, 5, 7, 10, 12]
+
 -- ====== TUNINGS ================================
-standard, dropD, halfDown, fullDown :: Notes
+standard, dropD, halfDown, fullDown, openE, sevenRock, ukulele:: Notes
 standard = [E, B, G, D, A, E]
 dropD    = [E, B, G, D, A, D]
 halfDown = [Eb, Bb, Gb, Db, Ab, Eb]
@@ -61,22 +65,19 @@ openE    = [E, B, Ab, E, B, E]
 sevenRock = [E, B, G, D, A, E, B]
 ukulele  = [A, E, C, G]
 
--- Generates a chromatic scale using the SingleNote passed in as the starting point.
-chromaticScale :: Int -> SingleNote -> Notes
-chromaticScale maxFret root = take (maxFret + 1) (iterate halfStepU root)
-
 -- ===============================================
 
 -- Generates a list of Steps for each note in the chord per string.
 fingering :: Notes -> Notes -> Int -> Int -> [Steps]
-fingering tuning scaleNotes fret maxFret =
-        map (sort . filter(fret<=)) [getPos scaleNotes | t <- tuning,
-            let fretPosition s = elemIndices s (chromaticScale maxFret t),
-            let getPos ss = concat [fretPosition s | s <- ss]]
+fingering tuning scaleNotes minFret maxFret =
+    map (sort . filter(minFret<=))
+        [fretPosition scaleNotes | t <- tuning,
+            let fretPosition ss =
+                    concat [elemIndices s (chromatic t maxFret) | s <- ss]]
 
 fingering2 :: Notes -> Notes -> Int -> Int -> [Steps]
-fingering2 tuning scaleNotes fret maxFret =
-    map (filterDifference . take 2) $ fingering tuning scaleNotes fret maxFret
+fingering2 tuning scaleNotes minFret maxFret =
+    map (filterDifference . take 2) $ fingering tuning scaleNotes minFret maxFret
     where
         filterDifference :: Steps -> Steps
         filterDifference [] = []
@@ -110,8 +111,8 @@ buildTab nt = do
     return (b ++ genSpaces maxLen b ++ "-|")
     where
         buildTabStrings :: [(SingleNote, Steps)] -> [String]
-        buildTabStrings xs =
-            [formatNote (fst x) ++ "||" ++ showPositions (snd x) | x <- xs]
+        buildTabStrings nt =
+            [formatNote (fst n) ++ "||" ++ showPositions (snd n) | n <- nt]
 
         showPositions :: Steps -> String
         showPositions [] = "--x"
@@ -128,8 +129,8 @@ buildTab nt = do
 -- Build Fret Diagram with Symbols
 -- =========================================================
 buildFretSymbols :: [(SingleNote, Steps)] -> Int -> [String]
-buildFretSymbols nt maxFret =
-    [formatNote (fst ns) ++ fretSymbols maxFret (snd ns) | ns <- nt]
+buildFretSymbols nts maxFret =
+    [formatNote (fst nt) ++ fretSymbols maxFret (snd nt) | nt <- nts]
     where
         fretSymbols :: Int -> Steps -> String
         fretSymbols maxFret [] = "|x|" ++ concat (replicate maxFret "-----|")
@@ -146,29 +147,30 @@ buildFretSymbols nt maxFret =
 -- =========================================================
 -- Build Fret Diagram with Notes
 -- =========================================================
-buildFretNotes :: SingleNote -> [(SingleNote, Steps)] -> Int -> [String]
-buildFretNotes root nt maxFret =
-    [formatNote (fst ns) ++ fretNotes (fst ns) root maxFret (snd ns) | ns <- nt]
+buildFretNotes :: [(SingleNote, Steps)] -> Int -> [String]
+buildFretNotes nts maxFret =
+    [formatNote (fst nt) ++ fretNotes maxFret nt | nt <- nts]
     where
-        fretNotes :: SingleNote -> SingleNote -> Int -> Steps -> String
-        fretNotes str root maxFret [] = "|x|" ++ concat (replicate maxFret "-----|")
-        fretNotes str root maxFret (n:ns)
-            | n == 0 = "|o|" ++ makeStringNotes str root maxFret ns
-            | otherwise = "| |" ++ makeStringNotes str root maxFret (n:ns)
+        fretNotes :: Int -> (SingleNote, Steps) -> String
+        fretNotes maxFret (_, []) = "|x|" ++ concat (replicate maxFret "-----|")
+        fretNotes maxFret (base, n:ns)
+            | n == 0 = "|o|" ++ makeStringNotes maxFret base ns
+            | otherwise = "| |" ++ makeStringNotes maxFret base (n:ns)
 
-        makeStringNotes :: SingleNote -> SingleNote -> Int -> Steps -> String
-        makeStringNotes str root maxFret ns = do
+        makeStringNotes :: Int -> SingleNote -> Steps -> String
+        makeStringNotes maxFret base ns = do
             fret <- [1..maxFret]
             if fret `elem` ns then
-                if root `elem` keysWithSharps then "--" ++ flatSharp (xSteps str fret) ++ "-|"
-                else "--" ++ formatNote (xSteps str fret) ++ "-|"
+                if base `elem` keysWithSharps then
+                    "--" ++ flatSharp (nSteps base fret) ++ "-|"
+                else "--" ++ formatNote (nSteps base fret) ++ "-|"
             else "-----|"
 
 -- =========================================================
 -- Helper functions to displayBoard the header and footer of the diagrams.
 -- =========================================================
 fretHeader :: SingleNote -> Int -> String
-fretHeader root fret = "\nRoot: " ++ show root ++ "\nMin. Fret: " ++ show fret
+fretHeader root minFret = "\nRoot: " ++ show root ++ "\nMin. Fret: " ++ show minFret
 
 fretFooter :: Int -> String
 fretFooter maxFret = do
@@ -186,30 +188,31 @@ buildGuitar tuning = do
     let maxFret = 12
     putStrLn $ "\n" ++ unlines (map putNotes (allNotes maxFret))
     where
-        allNotes maxFret = [chromaticScale maxFret n | n <- tuning]
+        allNotes :: Int -> [Notes]
+        allNotes maxFret = [chromatic n maxFret | n <- tuning]
 
 -- Generate one position for a chord/scale.
 buildSingle :: Notes -> (SingleNote -> Notes) -> SingleNote -> IO()
 buildSingle tuning scale root = do
     clearScreen
-    fret <- getFret
-    let maxFret = fret + 4
-        nt = zip tuning (fingering2 tuning (scale root) fret maxFret)
-        wrap = wrapBoard root fret maxFret
+    minFret <- getFret
+    let maxFret = minFret + 4
+        nt = zip tuning (fingering2 tuning (scale root) minFret maxFret)
+        wrap = wrapBoard root minFret maxFret
     menu [("1", "Tab", mapM_ putStrLn (buildTab nt)),
           ("2", "Symbols", wrap(buildFretSymbols nt maxFret)),
-          ("3", "Notes", wrap(buildFretNotes root nt maxFret))]
+          ("3", "Notes", wrap(buildFretNotes nt maxFret))]
 
 -- Generates all notes for a chord/scale, from fret 0 to the 16th fret.
 buildAll :: Notes -> (SingleNote -> Notes) -> SingleNote -> IO()
 buildAll tuning scale root = do
     clearScreen
-    let fret = 0
+    let minFret = 0
         maxFret = 16
-        nt = zip tuning (fingering tuning (scale root) fret maxFret)
-        wrap = wrapBoard root fret maxFret
+        nt = zip tuning (fingering tuning (scale root) minFret maxFret)
+        wrap = wrapBoard root minFret maxFret
     menu [("1", "Symbols", wrap (buildFretSymbols nt maxFret)),
-          ("2", "Notes", wrap(buildFretNotes root nt maxFret))]
+          ("2", "Notes", wrap(buildFretNotes nt maxFret))]
 
 -- =========================================================
 -- Helper functions for IO actions.
@@ -232,14 +235,14 @@ getFret = do
     putStr "\nEnter minimum fret (0 - 12): "
     maybeFret <- getLine
     case readMaybe maybeFret of
-        Just fret | fret >= 0 && fret <= 12 -> return fret
+        Just minFret | minFret >= 0 && minFret <= 12 -> return minFret
         _ -> do
             putStrLn "\nInvalid fret choice...Try again!"
             getFret
 
 wrapBoard :: SingleNote -> Int -> Int -> [String] -> IO()
-wrapBoard root fret maxFret result =
-    displayBoard([fretHeader root fret] ++ result ++ [fretFooter maxFret])
+wrapBoard root minFret maxFret result =
+    displayBoard([fretHeader root minFret] ++ result ++ [fretFooter maxFret])
 
 displayBoard :: [String] -> IO()
 displayBoard = mapM_ putStrLn
